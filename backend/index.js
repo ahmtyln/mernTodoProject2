bc53c73c092b138d9b5e8aa8eb24a6838f4480d5
ahmet <ahmtyln.06@gmail.com>
2023-04-09 23:50:56 +0200

    cors

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,14 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import allRoutes from './routes/index.js';
 
+const corsOptions = {
+    origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
+
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser());
