97f1dddcf48948d06a9b49076fe9ed202bd63fca
ahmet <ahmtyln.06@gmail.com>
2023-04-11 15:36:23 +0200

    pro

--- backend/index.js
+++ backend/index.js
@@ -17,17 +17,6 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 // routes
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
 
 app.use('/api', allRoutes);
 app.use((err,req,res,next) =>{
